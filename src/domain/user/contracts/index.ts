270f61f92e2cb3cbb43fe3bd14be2038f67bbe60
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-17 22:47:29 +0200

    refactore: cahnge name of types

---
+++ src/domain/user/contracts/index.ts
@@ -0,0 +1,17 @@
+export type userStore = {
+  id: string;
+  name: string;
+  email: string;
+  created_at: string;
+};
+export type userCreate = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+};
+
+export type user = {
+  name: string;
+  email: string;
+};
