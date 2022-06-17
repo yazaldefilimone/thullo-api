8c7c43ecdbaf436f1adeb01fc6be11b4043e2db9
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-17 21:23:15 +0200

    feat: create protocols of user

---
+++ src/domain/user/protocols/index.ts
@@ -0,0 +1,17 @@
+export type userStore = {
+  id: string;
+  name: string;
+  email: string;
+  created_at: string;
+};
+export type user = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+};
+
+export type userCreate = {
+  name: string;
+  email: string;
+};
