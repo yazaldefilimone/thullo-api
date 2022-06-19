b247f74af7eed7cd61c2905bf4482459a7571b86
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-19 18:21:30 +0200

    test: ensure return false if not receive correct name

---
+++ tests/shared/validators/index.spec.ts
@@ -0,0 +1,27 @@
+import { isValidPassword, isValidName, isValidEmail } from '@/shared/validators';
+
+function makeSut() {
+  const isValid = {
+    password: isValidPassword,
+    name: isValidName,
+    email: isValidEmail,
+  };
+  const fakeData = {
+    name: 'Joe Doe',
+    email: 'joedoe@gmail.com',
+    password: 'joedoepass',
+  };
+  return {
+    isValid,
+    fakeData,
+  };
+}
+
+describe('Validation', () => {
+  it('Should return false if not receive correct name', () => {
+    const { isValid, fakeData } = makeSut();
+    fakeData.name = '';
+    const response = isValid.name(fakeData.name);
+    expect(response).toEqual(false);
+  });
+});
