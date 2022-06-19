f76d6824e0a0e4008b4714dfc4f8e1ffe8d2fd38
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-19 18:05:04 +0200

    feat: create validade password

--- src/domain/user/contracts/index.ts
+++ src/domain/user/contracts/index.ts
@@ -2,16 +2,19 @@ export type userStore = {
   id: string;
   name: string;
   email: string;
+  password: string;
   created_at: string;
 };
 export type userCreate = {
   id: string;
   name: string;
   email: string;
+  password: string;
   createdAt: Date;
 };
 
 export type user = {
   name: string;
   email: string;
+  password: string;
 };
--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -3,35 +3,45 @@ import { buildResponse } from './protocols';
 import { user } from '@/domain/user/contracts';
 
 import { Either, left, right } from '@/shared/error-handler/either';
-import { isValidEmail, isValidName } from '@/shared/validators';
+import { isValidEmail, isValidName, isValidPassword } from '@/shared/validators';
 
 import { v4 as uuid } from 'uuid';
 
 export class User {
   public isValidEmail(email: string): Either<InvalidParamError, string> {
-    return isValidEmail(email) ? right(email) : left(new InvalidParamError(email));
+    return isValidEmail(email) ? right(email) : left(new InvalidParamError('email'));
   }
 
   public isValidName(name: string): Either<InvalidParamError, string> {
-    return isValidName(name) ? right(name) : left(new InvalidParamError(name));
+    return isValidName(name) ? right(name) : left(new InvalidParamError('name'));
+  }
+
+  public isValidPassword(password: string): Either<InvalidParamError, string> {
+    return isValidPassword(password) ? right(password) : left(new InvalidParamError('password'));
   }
 
   public build(user: user): buildResponse {
     const userValid = {
       name: this.isValidName(user.name),
       email: this.isValidEmail(user.email),
+      password: this.isValidPassword(user.password),
     };
+
     if (userValid.email.isLeft()) {
       return left(userValid.email.value);
     }
     if (userValid.name.isLeft()) {
       return left(userValid.name.value);
     }
+    if (userValid.password.isLeft()) {
+      return left(userValid.password.value);
+    }
 
     return right({
       id: uuid(),
       name: userValid.name.value as string,
       email: userValid.email.value as string,
+      password: userValid.email.value as string,
     });
   }
 }
