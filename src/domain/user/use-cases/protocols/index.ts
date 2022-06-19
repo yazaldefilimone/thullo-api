c965addff49b241c63f42fbd920b80a0e0826c66
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-19 19:39:48 +0200

    reafectore: add return id method create

--- src/domain/user/use-cases/protocols/index.ts
+++ src/domain/user/use-cases/protocols/index.ts
@@ -1,7 +1,6 @@
 import { AlreadyExistsError } from '@/domain/errors';
 import { InvalidParamError } from '@/domain/errors/InvalidParamError';
 import { Either } from '@/shared/error-handler/either';
-import { userCreate } from '@/domain/user/contracts';
 
 type createUserProtocolsFailed = InvalidParamError | AlreadyExistsError;
-export type createUserProtocols = Either<createUserProtocolsFailed, userCreate>;
+export type createUserProtocols = Either<createUserProtocolsFailed, { id: string }>;
